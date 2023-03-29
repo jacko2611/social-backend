667cb87a8d224111576b3cf04136d59e0a9df654
jacko2611 <jacksonkent70@gmail.com>
2023-03-29 21:49:22 +1100

    added reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { Schema } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: 'You need to provide a reaction!',
+            minlength: 1,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: 'You need to provide a username!'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+module.exports = ReactionSchema;
